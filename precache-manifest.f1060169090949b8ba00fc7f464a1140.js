a783299588d7c9b81b70095d6344a1a3f9c51153
emasovic <ekobree@live.com>
2018-12-08 14:18:42 +0100

    Updates

---
+++ precache-manifest.f1060169090949b8ba00fc7f464a1140.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b83c24e9b2e37ff2a43caa71d88a890",
+    "url": "/emasovic/cv/static/media/paper.1b83c24e.jpg"
+  },
+  {
+    "revision": "5f84d298a24aed3bd8963ace466cb8d5",
+    "url": "/emasovic/cv/static/media/me.5f84d298.png"
+  },
+  {
+    "revision": "d328be9b05d30a92aefc0dc5050c39fb",
+    "url": "/emasovic/cv/static/media/danger.d328be9b.png"
+  },
+  {
+    "revision": "c83342e6ac276eaa544be2051f826745",
+    "url": "/emasovic/cv/static/media/cover.c83342e6.png"
+  },
+  {
+    "revision": "b80a025d905237ab081f",
+    "url": "/emasovic/cv/static/js/runtime~main.b80a025d.js"
+  },
+  {
+    "revision": "5a9c9f87930158b9bd34",
+    "url": "/emasovic/cv/static/js/main.5a9c9f87.chunk.js"
+  },
+  {
+    "revision": "aa1bc1880c92227f958f",
+    "url": "/emasovic/cv/static/js/1.aa1bc188.chunk.js"
+  },
+  {
+    "revision": "5a9c9f87930158b9bd34",
+    "url": "/emasovic/cv/static/css/main.8e17384c.chunk.css"
+  },
+  {
+    "revision": "14bfbdb1237369122818e93085fe8834",
+    "url": "/emasovic/cv/index.html"
+  }
+];
